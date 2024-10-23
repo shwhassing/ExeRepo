6edba44faf5d7d95f670e8e323916775e8c406dd
Sverre Hassing <s.h.w.hassing@tudelft.nl>
2024-10-23 15:25:50 +0200

    Added addition and subtraction functions

---
+++ example.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 15:24:07 2024.
+
+@author: shwhassing
+"""
+
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def subtract(a, b):
+    return a + b  # <--- fix this in step 8
+
+
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
